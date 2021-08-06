0c615957c9b299fbecf50846527821c38852594f
bbs3.hunglt <bbs3.hunglt@techcombank.com.vn>
2021-08-07 00:01:18 +0700

    write testcase

---
+++ demo/src/test/java/com/example/demo/service/NhaCungCapServiceTest.java
@@ -0,0 +1,40 @@
+package com.example.demo.service;
+
+import com.example.demo.controller.NhaCungCapController;
+import com.example.demo.model.NhaCungCap;
+import com.example.demo.repo.NhaCungCapRepo;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class NhaCungCapServiceTest {
+
+    @InjectMocks
+    NhaCungCapController nhaCungCapController;
+
+    @Mock
+    NhaCungCapRepo nhaCungCapRepo;
+
+    @Test
+    public void getAllNhaCungCap(){
+        List<NhaCungCap> nhaCungCapList = new ArrayList<>();
+        NhaCungCap nhaCungCap = new NhaCungCap();
+        nhaCungCap.setTenNhaCungCap("ten test");
+        nhaCungCap.setSoDienThoai("0987654321");
+        nhaCungCap.setFax("fax@test");
+        nhaCungCap.setEmail("mail@gmail.com");
+
+        nhaCungCapList.add(nhaCungCap);
+        when(nhaCungCapRepo.findAll()).thenReturn(nhaCungCapList);
+        assertEquals(1,nhaCungCapList.size());
+
+    }
+}
